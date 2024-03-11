de81d40e0922aa098c9118ca4a3d8f6a5082282e
Gabriela Inácio <gabrielainacio8491@gmail.com>
2024-03-11 14:03:51 -0300

    exercicios

---
+++ fatoriallonglong.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    long long int fat = 1;
+    int num, i;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    for (i = 1; i <= num; i++) {
+        fat = i * fat;
+    }
+
+    printf("O fatorial de %d eh: %lld\n", num, fat); 
+
+    return 0;
+}
+
+
